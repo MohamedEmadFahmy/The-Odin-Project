c19fb1aee07812a27a00cadaf826705499a71c09
MohamedEmadFahmy <mohamedemadfahmy0@gmail.com>
2023-02-21 22:55:31 +0200

    Finish 1st and 2nd js fundamentals (excluding regex)

---
+++ js/index.js
@@ -0,0 +1,37 @@
+// console.log("Hello, World")
+// console.log(23 + 97)
+// console.log((4 + 6 + 9) / 77)
+// let a = 10
+// console.log(a)
+// let max = 57
+// let actual = max - 13
+// let percentage = actual / max
+
+// ---------------------------
+// part 2
+// ---------------------------
+// const sglDbl = 'Would you eat a "fish supper"?';
+// const dblSgl = "I'm feeling blue.";
+// console.log(sglDbl);
+// console.log(dblSgl);
+// const bigmouth = 'I\'ve got no right to take my place…';
+// console.log(bigmouth);
+
+// const myName = "Chris";
+// const myName = "Mohamed";
+// const greeting = `Hello, ${myName}`;
+// console.log(greeting); // "Hello, Chris"
+
+// const output = `I like the song.
+// I gave it a score of 90%.`;
+// console.log(output);
+
+// alert( '2' > 1 ); // true, string '2' becomes a number 2
+// alert( '01' == 1 ); // true, string '01' becomes a number 1
+
+// alert( null > 0 );  // (1) false
+// alert( null == 0 ); // (2) false
+// alert( null >= 0 ); // (3) true
+
+// alert( null === undefined ); // false
+// alert( null == undefined ); // true
