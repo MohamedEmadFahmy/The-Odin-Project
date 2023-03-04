c55502559151d111c5b6abdb89b92562a08fef2d
MohamedEmadFahmy <mohamedemadfahmy0@gmail.com>
2023-03-04 02:09:27 +0200

    Add js exercises folders in Fundamentals Part 4 solve all the required exercises:     helloWorld     repeatString     reverseString     removeFromArray     sumAll     leapYears     tempConversion

---
+++ js/javascript-exercises-main/07_tempConversion/tempConversion.js
@@ -0,0 +1,15 @@
+const convertToCelsius = function(temp) {
+  temp = (temp - 32) * (5/9);
+  return Math.round (temp * 10) / 10;
+};
+
+const convertToFahrenheit = function(temp) {
+  temp = temp * (9/5) + 32;
+  return Math.round (temp * 10) / 10;
+};
+
+// Do not edit below this line
+module.exports = {
+  convertToCelsius,
+  convertToFahrenheit
+};
